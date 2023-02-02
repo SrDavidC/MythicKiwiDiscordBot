d05761fe76d78ed3c2574fae47f06852867374bc
David Cerdas <david.cerdasalvarado@ucr.ac.cr>
2023-02-02 10:59:11 -0600

    Init commit

---
+++ src/commands/tools/ping.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder, InteractionResponse } = require('discord.js');
+const { execute } = require('../../events/client/ready');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Return my ping'),
+    async execute(interaction, client) {
+        const message = await interaction.deferReply({
+            fetchReply: true
+        });
+
+        const newMessage = `API Latency: ${client.ws.ping}\nClient ping: ${message.createdTimestamp - interaction.createdTimestamp}`
+        await interaction.editReply({
+            content: newMessage
+        });
+    }
+}
