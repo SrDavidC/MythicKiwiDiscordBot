2b5fd1047d8aed163c09c005b52a12cd0d028c8a
David Cerdas <david.cerdasalvarado@ucr.ac.cr>
2023-02-05 11:44:02 -0600

    Added minecraft skins command (skin, render and head)

---
+++ src/commands/tools/skin.js
@@ -0,0 +1,54 @@
+const { SlashCommandBuilder, EmbedBuilder, MessageEmbed } = require('discord.js');
+const request = require("request");
+
+
+function random(colors) {
+    return colors[Math.floor(Math.random() * colors.length)];
+  };
+  
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('skin')
+        .setDescription('Returns skin of a minecraft user')
+        .addStringOption(option =>
+            option
+                .setName("username")
+                .setDescription("username to get minecraft skin")
+                .setRequired(true))
+    , async execute(interaction, client) {
+        const username = interaction.options.getString('username');
+        if (username.length > 16) {
+            await interaction.reply({
+                content: 'Minecraft usernames have less than 16 characters!'
+            });
+        }
+        let mojang_player_api = `https://api.mojang.com/users/profiles/minecraft/${username}`;
+        request(mojang_player_api, async function (err, resp, body) {
+            if (err) {
+                interaction.reply({
+                    content: `Username: **${nombre}** is not a premium user`
+                });
+            }
+            try {
+                body = JSON.parse(body);
+                let player_id = body.id;
+                let skin = `https://crafatar.com/skins/${player_id}`;
+                let embed = new EmbedBuilder()
+                    .setTitle(`${username.toUpperCase()} Skin`)
+                    .setColor(random(['#008000', '#E50000', '#99AAb5','#C27C0E']))
+                    .setFooter({
+                        iconURL: interaction.user.displayAvatarURL({ dynamic: true }),
+                        text: `${interaction.user.tag}`
+                    })
+                    .setImage(skin);
+                await interaction.channel.send({embeds: [ embed]});
+                await interaction.reply('Skin has been sent succesfully');
+            } catch (err) {
+                interaction.reply({
+                    content: 'An error was occurred'
+                });
+                console.log(err);
+            }
+        });
+    }
+}
