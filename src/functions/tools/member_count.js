589c1ddf42f49b9c9a7dc616deb63eeb86cbb99c
David C <david.cerdasalvarado@ucr.ac.cr>
2023-03-02 22:08:00 -0600

    Change guild in membercount from .env to config

--- src/functions/tools/member_count.js
+++ src/functions/tools/member_count.js
@@ -1,12 +1,12 @@
 module.exports = async (client) => {
     client.memberCount = async (client) => {
-        const guild = client.guilds.cache.get(process.env.guildID1);
+        const guild = client.guilds.cache.get(client.config.guild_membercount);
         setInterval(() => {
             const memberCount = guild.memberCount;
             const channel = guild.channels.cache.get(process.env.member_count_channel);
             if (channel) {
                 channel.setName(`◜Members◝・${memberCount.toLocaleString()} ・`);
-                console.log('Updating Member Count');
+                // console.log('Updating Member Count');
             }
         }, 120000);
     }
